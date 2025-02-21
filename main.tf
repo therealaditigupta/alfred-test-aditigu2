f34692a7579b1a4e9bcfd01a83e3ba86b02c9a55
Aditi Gupta <54046563+therealaditigupta@users.noreply.github.com>
2025-02-21 16:55:02 -0500

    Create a new VPC code

--- main.tf
+++ main.tf
@@ -12,6 +12,15 @@ resource "aws_vpc" "main_vpc" {
   }
 }
 
+# Create a VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main_vpc"
+  }
+}
+
 # Create public and private subnets
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
