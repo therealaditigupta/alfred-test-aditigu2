c59e33da78d65b456533cbe35000cd27b6d5f000
Aditi Gupta <54046563+therealaditigupta@users.noreply.github.com>
2025-03-24 17:15:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,6 +22,16 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.2.0/24"
